dff27a3486ed2d1c190c787521023e8c9e0c708b
ARGONATH\Diego <dswanderley@gmail.com>
2017-12-22 17:38:40 +0000

    Interception inside triangle bug fixed. Include points in line.

--- interception.m
+++ interception.m
@@ -11,28 +11,78 @@ p_0 = plane(1,:);
 p_1 = plane(2,:);
 p_2 = plane(3,:);
 
-
 p_out = [];
 
 M = [(p_dif)', (p_1 - p_0)', (p_2 - p_0)'];
+% Check if cross the plane
 if det(M) ~= 0
     t_u_v = M^-1 * (p_a - p_0)';
-    
+    % Calculate point of interception
     p_cross = p_a - p_dif * t_u_v(1);
-    
-    if p_cross(1) <= max(plane(:,1)) && p_cross(1) >= min(plane(:,1))
-        if p_cross(2) <= max(plane(:,2)) && p_cross(2) >= min(plane(:,2))
-            if p_cross(3) <= max(plane(:,3)) && p_cross(3) >= min(plane(:,3))
-                p_out = p_cross;
-                
-                
-if (p_a(2) == 0.5 && p_a(3) == 0.75)
-    disp(p_out);
-end
-            end
-        end
-    end
-    
+    % Verify if point inside the voxel triangle 
+    if (pointintriangle(p_cross, p_0, p_1, p_2))
+        p_out = p_cross;
+    end    
 end
 
 end
+
+function [valid] = sameside(p1,p2,a,b)
+
+    cp1 = cross(b-a, p1-a);
+    cp2 = cross(b-a, p2-a);
+    
+    if (dot(cp1, cp2) >= 0)
+        valid = true;
+    else
+        valid = false;
+    end
+end
+
+% function inline = isinline(p, a, b)
+%     
+%     v = b - a;
+%     t = (p - a)./v;
+%     
+%     if (t(1) == t(2) && t(2) == t(3))
+%         inline = true;
+%     else
+%         inline = false;
+%     end
+% 
+% end
+
+function [inside] = pointintriangle(p, a, b, c)
+
+    if (sameside(p,a,b,c) && sameside(p,b,a,c) && sameside(p,c,a,b))
+        inside = true;
+    else        
+%         if (isinline(p, a, b) || isinline(p, b, c) || isinline(p, c, a))
+%             inside = true;
+%         else
+            inside = false;
+%         end
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
