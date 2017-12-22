a1da73291ba66ee5be3efc62a5281b480641c56b
ARGONATH\Diego <dswanderley@gmail.com>
2017-12-22 18:05:15 +0000

    Max angle opening is the double of the calculate angle based in the center.

--- getfocalpoints.m
+++ getfocalpoints.m
@@ -3,20 +3,21 @@ function [ focus ] = getfocalpoints( sensor, center, u, v, orientation, max_angl
 %   Detailed explanation goes here
 
 if (max_angle == 0)
-   
+    % If collinear each point has a focus
     ori = repmat(orientation, size(sensor,1), 1);
     focus = sensor - ori;
-    
 else
-
+    % Angle is half of the max_ange (opening)
+    angle = max_angle/2;
+    % Calculate distance from the center
     aux = repmat(center, size(sensor,1), 1);
     d = sum((sensor - aux).^2, 2);
-
+    % Get index for the maximun distance
     idx = find(d == max(d), 1);
     p_max = sensor(idx,:);
-    
-    pf = focalpoint(center, p_max, max_angle, u, v);
-    
+    % Get focal point
+    pf = focalpoint(center, p_max, angle, u, v);
+    % Replicate this point for n points in sensor
     focus = repmat(pf, size(sensor,1), 1);
 end
 
