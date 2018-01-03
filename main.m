d8602fb479c1ef16589318dbe7a69cc04cb4ce1d
Diego Wanderley <dswanderley@gmail.com>
2018-01-03 19:44:29 +0000

    Plot center point of each sensor

--- main.m
+++ main.m
@@ -22,26 +22,28 @@ u = [0, 1, 0];          % Array plan vector 1
 v = [0, 0, 1];          % Array plan vector 2
 % Rectangular sensor
 sensor_size = [5, 3];   % unit (number of beams)
+sensor_beams = sensor_size(1) * sensor_size(2);
 array_dim = [90, 160]/10;  % cm
-array_size = [5, 3];    % unit (number of sensors in each direction)
+array_size = [3, 5];    % unit (number of sensors in each direction)
+num_sensors = array_size(1) * array_size(2);        % Number of sensors on array
 % Get sensors
 [sensors, centers] = sensorarray(sensor_size, array_dim, array_size, u, v, [0,0,0]);
 
 % GET FOCAL POINTS
-center = centers(:,:,1);
+array_center = centers(:,:,1);
 sensor_base = sensors(:,:,1);
 % Angle - 0 for collinear
 max_angle = 0;%30 * pi /180;
 % Focus based to be applied in each sensor
-focus_base = getfocalpoints(sensor_base, center, u, v, orientation, max_angle);
+focus_base = getfocalpoints(sensor_base, array_center, u, v, orientation, max_angle);
 % Remove offset from sensor_base 
-sensor_base(:,1) = sensor_base(:,1) - center(1);
-sensor_base(:,2) = sensor_base(:,2) - center(2);
-sensor_base(:,3) = sensor_base(:,3) - center(3);
+sensor_base(:,1) = sensor_base(:,1) - array_center(1);
+sensor_base(:,2) = sensor_base(:,2) - array_center(2);
+sensor_base(:,3) = sensor_base(:,3) - array_center(3);
 % Remove offset from focus_base
-focus_base(:,1) = focus_base(:,1) - center(1);
-focus_base(:,2) = focus_base(:,2) - center(2);
-focus_base(:,3) = focus_base(:,3) - center(3);
+focus_base(:,1) = focus_base(:,1) - array_center(1);
+focus_base(:,2) = focus_base(:,2) - array_center(2);
+focus_base(:,3) = focus_base(:,3) - array_center(3);
 
 focus = repmat(focus_base, 1, 1, size(sensors,3));
 focus(:,1,:) = focus(:,1,:) + repmat(centers(:,1,:), size(focus(:,1,:),1), 1, 1);
@@ -51,9 +53,16 @@ focus(:,3,:) = focus(:,3,:) + repmat(centers(:,3,:), size(focus(:,3,:),1), 1, 1)
 figure, % PLOT REAL WORLD
 trisurf(obj.f.v,obj.v(:,1),obj.v(:,2),obj.v(:,3),'Facecolor','red','FaceAlpha',0.1)
 hold on
-% plot3(sensors(:,1,16), sensors(:,2,16), sensors(:,3,16), '.')
+% PLOT SENSOR CENTERS
+p_x = reshape(centers(:,1,:), 1, num_sensors); 
+p_y = reshape(centers(:,2,:), 1, num_sensors);
+p_z = reshape(centers(:,3,:), 1, num_sensors);
+plot3(p_x, p_y, p_z, '*')
 xlabel('x'); ylabel('y'); zlabel('z');
 axis equal
+% PLOT SENSOR BORDERS
+
+
 
 %%% RAYS DISTANCE %%%
 array_dists = zeros(1,size(sensors,3));
@@ -62,16 +71,13 @@ for k = 1:size(sensors,3)
     % Get all point in each sensor, their focus and the sensor center
     sensor = sensors(:,:,k);
     fc = focus(:,:,k);
-    center = centers(:,:,k);
+    c = centers(:,:,k);
     % Calculate minimal distance from center
-    [min_dist, p_sensor, p_voxel] = sensormindist(sensor, fc, center, obj);
+    [min_dist, p_sensor, p_voxel] = sensormindist(sensor, fc, c, obj);
 	array_dists(:,k) = min_dist;
-    line = [center; p_voxel]; 
+    line = [c; p_voxel]; 
     array_lines(:,:,k) = line;
-    
-    
-    
-    plot3(center(1), center(2), center(3), '*');
+    % PLOT MIN BEAM  
     plot3(line(:,1,:), line(:,2,:), line(:,3,:));
 end
 
